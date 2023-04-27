d96b550258d255fd6a8b877dd54bfabe70a11429
NeevtheGreat875 <75111555+NeevtheGreat875@users.noreply.github.com>
2023-04-27 20:14:47 +0530

    Update Glaucoma_Classifier.py

--- pages/Glaucoma_Classifier.py
+++ pages/Glaucoma_Classifier.py
@@ -8,7 +8,7 @@ st.set_page_config(
     page_icon=":dna:",
     initial_sidebar_state="expanded",
 )
-st.title("Glaucoma Predictor with OCT Scans of eyes")
+st.title("Glaucoma Predictor")
 
 st.write("Glaucoma is a group of eye diseases that damage the optic nerve, which connects the eye to the brain. This damage can cause irreversible vision loss and blindness if left untreated. The most common type of glaucoma, open-angle glaucoma, occurs when fluid builds up in the eye and increases pressure on the optic nerve.")
 st.write("The problems caused by glaucoma include a gradual loss of peripheral (side) vision, which can go unnoticed until it becomes severe. In advanced stages, central vision can also be affected. While there is no cure for glaucoma, early detection and treatment can help slow or prevent vision loss. Treatment may include eye drops, medication, laser surgery, or traditional surgery to lower the pressure in the eye.")
