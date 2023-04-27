c10041ed91ae2a5bf9b63370d087101e619dc2fa
NeevtheGreat875 <75111555+NeevtheGreat875@users.noreply.github.com>
2023-04-27 23:47:23 +0530

    Update Glaucoma_Classifier.py

--- pages/Glaucoma_Classifier.py
+++ pages/Glaucoma_Classifier.py
@@ -22,7 +22,7 @@ components.html(
             padding:0px;
             color: white;
             font-family: "Source Sans Pro", sans-serif;
-            font-size: 55px;
+            font-size: max(8vw, 20px);
             font-weight: 700;
             top: 0px;
             right: 25%;
@@ -31,7 +31,7 @@ components.html(
     </style>
     <p id="effect">HealthVision AI</p>
     """,
-    height=60,
+    height=69,
 )
 st.title("Glaucoma Predictor")
 
