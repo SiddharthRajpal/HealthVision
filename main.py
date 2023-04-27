a19226fba51c1ca8527720d6d49c971251f14bbe
SiddharthRajpal <siddharthrajpal09@gmail.com>
2023-04-27 11:53:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ import streamlit as st
 import pandas as pd
 from io import StringIO
 import imagerec
+st.title("X-Ray Characterization")
 uploaded_file = st.file_uploader("Choose a File", type=['jpg','png','jpeg'])
 x = st.button("Predict")
 if x:
-    y= imagerec.imagerecognise(uploaded_file,"Models/FinalModel.h5","Models/labels.txt")
-    print(y)
+    with st.spinner("Predicting..."):
+        y= imagerec.imagerecognise(uploaded_file,"Models/FinalModel.h5","Models/labels.txt")
+    st.write(y)
