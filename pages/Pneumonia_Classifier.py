369d2dc45e136144e827fdb797540513452fa764
SiddharthRajpal <siddharthrajpal09@gmail.com>
2023-04-27 21:17:11 +0530

    Update Pneumonia_Classifier.py

--- pages/Pneumonia_Classifier.py
+++ pages/Pneumonia_Classifier.py
@@ -3,16 +3,17 @@ import imagerec
 import pandas as pd
 import random
 import streamlit.components.v1 as components
-st.markdown(""" <style>
-#MainMenu {visibility: hidden;}
-footer {visibility: hidden;}
-</style> """, unsafe_allow_html=True)
+
 
 st.set_page_config(
     page_title="HealthVision AI",
     page_icon=":dna:",
     initial_sidebar_state="expanded",
 )
+st.markdown(""" <style>
+#MainMenu {visibility: hidden;}
+footer {visibility: hidden;}
+</style> """, unsafe_allow_html=True)
 components.html(
     """
     <style>
