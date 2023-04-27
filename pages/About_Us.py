204b0a2abdc0e4b660d8b48f2f12a885ab85d9a1
SiddharthRajpal <siddharthrajpal09@gmail.com>
2023-04-28 00:17:11 +0530

    Update About_Us.py

--- pages/About_Us.py
+++ pages/About_Us.py
@@ -15,21 +15,20 @@ img{border-radius: 10px;}
 # Define Neev Datta and Siddharth Rajpal
 neev = {
     'name': 'Neev Datta',
-    'title': 'Co-Founder',
+    'title': '',
     'bio': 'I am a computer geek who loves coding, creating 3D art, and developing games. Over the last 2 years, I have mastered Python, C#, C++, HTML, CSS, Java, JavaScript, Dart, Rust and C. One of my specialities is creating entirely computer-generated scenes in Blender, I am so passionate about it that I even have an Instagram page dedicated solely to showcasing my creations. I also have invested many hours into mastering Unreal Engine and Unity. Ignoring computers I am also an aspiring professional racer in the Indian F4 Championship.',
 
 }
 
 siddharth = {
     'name': 'Siddharth Rajpal',
-    'title': 'Co-Founder',
+    'title': 'Founders',
     'bio': 'In my leisure time, I enjoy coding and possess proficiency in eight programming languages. Additionally, I indulge in reading books and have a firm grasp of mathematics and physics. Other than that I have a keen interest in AI. In addition to my coding expertise, I am also keenly interested in 3D art and have honed my skills with the Blender software. I am so passionate about it that I even have an Instagram page dedicated solely to showcasing my creations. Furthermore, I love 3D printing technology and enjoy experimenting with it whenever possible.',
 }
 
 # Define page layout
 st.write(f"# About Us")
-st.write(f"## {neev['name']} and {siddharth['name']}")
-st.write(f"### {neev['title']} and {siddharth['title']}")
+st.write(f"## Founders")
 
 # Add photos
 col1, col2 = st.columns(2)
