dca2c81d3ee810d42d5812a4d4faabbd8fe01c0f
SiddharthRajpal <siddharthrajpal09@gmail.com>
2023-04-28 11:26:26 +0530

    Update Pneumonia_Classifier.py

--- pages/Pneumonia_Classifier.py
+++ pages/Pneumonia_Classifier.py
@@ -36,7 +36,9 @@ components.html(
 st.title("Pneumonia X-Ray Predictor")
 
 st.write("Pneumonia is an infection that inflames the air sacs in one or both lungs, causing them to fill with fluid or pus. The infection can be caused by a variety of microorganisms, including bacteria, viruses, and fungi. Pneumonia can be acquired in the community or in a hospital setting, and it can range in severity from mild to life-threatening.")
+st.divider()
 st.write("Machine learning (ML) can play an important role in pneumonia prediction by analyzing medical images and identifying patterns that may be indicative of the disease. For example, ML models can be trained on large datasets of chest X-rays and CT scans to learn features that distinguish between normal lungs and those affected by pneumonia.")
+st.divider()
 st.write("We have developed A Convolutional Neural Network (CNN) to predict wether the lung scan has Pneumonia or not. It has been trained on more than 10,000 images divided into two classes, to upto 50 epochs.")
 
 uploaded_file = st.file_uploader("Choose a File", type=['jpg','png','jpeg'])
