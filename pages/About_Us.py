cd3469053c333c0be1ad8557c2949853a7ac9dff
NeevtheGreat875 <75111555+NeevtheGreat875@users.noreply.github.com>
2023-04-27 23:52:20 -0700

    Update About_Us.py

--- pages/About_Us.py
+++ pages/About_Us.py
@@ -40,7 +40,7 @@ components.html(
             font-size: max(8vw, 20px);
             font-weight: 700;
             top: 0px;
-            right: 25%;
+            right: 50%;
             position: fixed;
             background: -webkit-linear-gradient(0.25turn,#FF4C4B, #FFFB80);
             -webkit-background-clip: text;
