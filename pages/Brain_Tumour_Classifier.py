ee887924f3c01d5cd5e19f1d0e980effe4055a28
SiddharthRajpal <siddharthrajpal09@gmail.com>
2023-04-28 12:01:49 +0530

    Add files via upload

---
+++ pages/Brain_Tumour_Classifier.py
@@ -0,0 +1,62 @@
+import streamlit as st
+import imagerec
+import pandas as pd
+import random
+import streamlit.components.v1 as components
+
+st.set_page_config(
+    page_title="HealthVision AI",
+    page_icon=":dna:",
+    initial_sidebar_state="expanded",
+)
+
+st.markdown(""" <style>
+#MainMenu {visibility: hidden;}
+footer {visibility: hidden;}
+</style> """, unsafe_allow_html=True)
+components.html(
+    """
+    <style>
+        p{
+            margin:0px;
+            padding:0px;
+            color: white;
+            font-family: "Source Sans Pro", sans-serif;
+            font-size: max(8vw, 20px);
+            font-weight: 700;
+            top: 0px;
+            right: 25%;
+            position: fixed;
+        }
+    </style>
+    <p id="effect">HealthVision AI</p>
+    """,
+    height=69,
+)
+st.title("Brain Tumuor Predictor")
+
+st.write('<style>div.row-widget.stMarkdown { font-size: 24px; }</style>', unsafe_allow_html=True)
+
+
+st.write("""There are several types of brain tumors, including:
+
+Glioma: A type of tumor that originates in the glial cells, which are the supportive cells in the brain. Gliomas can be either low-grade (slow-growing) or high-grade (fast-growing) and can affect different parts of the brain.
+
+Meningioma: A tumor that arises from the meninges, which are the protective membranes that surround the brain and spinal cord. Meningiomas are usually benign and slow-growing, and may not require treatment if they are not causing symptoms.
+
+Pituitary adenoma: A tumor that develops in the pituitary gland, which is located at the base of the brain. Pituitary adenomas can affect hormone production and cause a variety of symptoms, depending on the hormones that are affected.")
+st.divider()
+st.write("The problems caused by glaucoma include a gradual loss of peripheral (side) vision, which can go unnoticed until it becomes severe. In advanced stages, central vision can also be affected. While there is no cure for glaucoma, early detection and treatment can help slow or prevent vision loss. Treatment may include eye drops, medication, laser surgery, or traditional surgery to lower the pressure in the eye.""")
+#st.divider()
+st.write("hence, we have developed A Convolutional Neural Network (CNN) to predict whether the MRI Scan of the brain has a tumour or not. It has been trained on more than 1000 images divided into four classes, to upto 50 epochs.")
+#st.divider()
+uploaded_file = st.file_uploader("Choose a File", type=['jpg','png','jpeg'])
+
+
+if uploaded_file!=None:
+    st.image(uploaded_file)
+x = st.button("Predict")
+if x:
+    with st.spinner("Predicting..."):
+        y,conf = imagerec.imagerecognise(uploaded_file,"Models/BrainTumuorModel.h5","Models/BrainTumuorLabels.txt")
+    st.header(f"It is {y}")
