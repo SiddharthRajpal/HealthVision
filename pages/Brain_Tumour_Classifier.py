ab363711e01697036f9ea45df9346ff2dda389e0
SiddharthRajpal <siddharthrajpal09@gmail.com>
2023-04-28 12:16:34 +0530

    Update Brain_Tumour_Classifier.py

--- pages/Brain_Tumour_Classifier.py
+++ pages/Brain_Tumour_Classifier.py
@@ -10,6 +10,7 @@ st.set_page_config(
     initial_sidebar_state="expanded",
 )
 
+
 st.markdown(""" <style>
 #MainMenu {visibility: hidden;}
 footer {visibility: hidden;}
@@ -17,22 +18,28 @@ footer {visibility: hidden;}
 components.html(
     """
     <style>
-        p{
+        #effect{
             margin:0px;
             padding:0px;
-            color: white;
             font-family: "Source Sans Pro", sans-serif;
             font-size: max(8vw, 20px);
             font-weight: 700;
             top: 0px;
             right: 25%;
             position: fixed;
+            background: -webkit-linear-gradient(0.25turn,#FF4C4B, #FFFB80);
+            -webkit-background-clip: text;
+            -webkit-text-fill-color: transparent;
+        }
+        p{
+            font-size: 2rem;
         }
     </style>
     <p id="effect">HealthVision AI</p>
     """,
     height=69,
 )
+
 st.title("Brain Tumuor Predictor")
 
 st.write('<style>div.row-widget.stMarkdown { font-size: 24px; }</style>', unsafe_allow_html=True)
@@ -47,9 +54,9 @@ Meningioma: A tumor that arises from the meninges, which are the protective memb
 Pituitary adenoma: A tumor that develops in the pituitary gland, which is located at the base of the brain. Pituitary adenomas can affect hormone production and cause a variety of symptoms, depending on the hormones that are affected.")
 st.divider()
 st.write("The problems caused by glaucoma include a gradual loss of peripheral (side) vision, which can go unnoticed until it becomes severe. In advanced stages, central vision can also be affected. While there is no cure for glaucoma, early detection and treatment can help slow or prevent vision loss. Treatment may include eye drops, medication, laser surgery, or traditional surgery to lower the pressure in the eye.""")
-#st.divider()
+st.divider()
 st.write("hence, we have developed A Convolutional Neural Network (CNN) to predict whether the MRI Scan of the brain has a tumour or not. It has been trained on more than 1000 images divided into four classes, to upto 50 epochs.")
-#st.divider()
+st.divider()
 uploaded_file = st.file_uploader("Choose a File", type=['jpg','png','jpeg'])
 
 
