c2f55f06838c1c9b3628f2ef4229a616a1361642
NeevtheGreat875 <75111555+NeevtheGreat875@users.noreply.github.com>
2023-04-28 01:17:25 -0700

    Update Skin_Cancer_Classifier.py

--- pages/Skin_Cancer_Classifier.py
+++ pages/Skin_Cancer_Classifier.py
@@ -59,4 +59,33 @@ x = st.button("Predict")
 if x:
     with st.spinner("Predicting..."):
         y,conf = imagerec.imagerecognise(uploaded_file,"Models/SkinCancerModel.h5","Models/SkinCancerLabel.txt")
-    st.header(f"It is {y} ")
+        
+    if y.strip() == "Benign":
+        components.html(
+            """
+            <style>
+            h1{
+                
+                background: -webkit-linear-gradient(0.25turn,#01CCF7, #8BF5F5);
+                -webkit-background-clip: text;
+                -webkit-text-fill-color: transparent;
+                font-family: "Source Sans Pro", sans-serif;
+            }
+            </style>
+            <h1>It is Benign</h1>
+            """
+        )
+    else:
+        components.html(
+            """
+            <style>
+            h1{
+                background: -webkit-linear-gradient(0.25turn,#FF4C4B, #F70000);
+                -webkit-background-clip: text;
+                -webkit-text-fill-color: transparent;
+                font-family: "Source Sans Pro", sans-serif;
+            }
+            </style>
+            <h1>Cancer Detected</h1>
+            """
+        )
